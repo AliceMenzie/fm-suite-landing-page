d1624d097aae215a9af54a4046bddcf9d109613a
AliceMenzie <alice.menzie@gmail.com>
2022-09-11 17:33:40 +1000

    add typography stories

--- src/components/atom/typography/Typography.stories.tsx
+++ src/components/atom/typography/Typography.stories.tsx
@@ -2,9 +2,23 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { Typography } from "./Typography";
 
-storiesOf("Design System/Atoms/Button", module).add("Primary", () => (
+storiesOf("Design System/Atoms/Typography", module).add("Heading 1", () => (
+  <Typography //type="button"
+    as="h1"
+  >
+    Hello
+  </Typography>
+));
+storiesOf("Design System/Atoms/Typography", module).add("Heading 2", () => (
   <Typography //type="button"
     as="h2"
+  >
+    Hello
+  </Typography>
+));
+storiesOf("Design System/Atoms/Typography", module).add("Paragraph", () => (
+  <Typography //type="button"
+    as="p"
   >
     Hello
   </Typography>
